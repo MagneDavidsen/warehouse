f5c36348903dec64f501789beeadb4c9379fe948
Magne Davidsen <magne.davidsen@gmail.com>
2013-06-06 15:17:00 +0200

    reverted to using cookies

--- src/main/scala/nbrno/Plans.scala
+++ src/main/scala/nbrno/Plans.scala
@@ -21,16 +21,13 @@ object RappersPlan extends Plan {
 
     case req@Path(Seg("api" ::"rappers" :: rapperId :: "vote" :: Nil)) =>
       val body : String = Body.string(req)
-
-      logger.info("RequestHeaders: " ++ body)
-
+      logger.info("RequestBody: " ++ body)
       req match {
       case POST(_) & Cookies(cookies) => req match {
         case RequestContentType("application/json;charset=UTF-8") => req match {
           case Accepts.Json(_) =>
             Ok ~> JsonContent ~> {
               val vote : Vote = read[Vote](body)
-              logger.info("SessionId: " ++ cookies("SESSION_ID").get.value)
               val user : Option[User] = NbrnoServer.getUserFromSessionStore(cookies("SESSION_ID").get.value)
               //TODO: Ensure rapperId is int
               if(user.isDefined){
@@ -66,8 +63,7 @@ object UserPlan extends Plan {
               if (DatabaseHandler.availableUsername(user.username)) {
                 val newUser : User = DatabaseHandler.createUser(user, req.remoteAddr)
                 val sessionId : String = NbrnoServer.addUserToSessionStore(newUser)
-                //TODO Use cookie for this, when I get Angular.js to work with responsecookies
-                Ok ~> ResponseString("{\"SESSION_ID\":\"" ++sessionId++ "\" }")
+                Ok ~> SetCookies(Cookie("SESSION_ID", sessionId))
               }
               else BadRequest ~> ResponseString("Username not available")
             }
@@ -90,9 +86,7 @@ object UserPlan extends Plan {
               val validatedUser = DatabaseHandler.validateUser(user.username, user.password.get)
               if (validatedUser.isDefined) {
                 val sessionId : String = NbrnoServer.addUserToSessionStore(validatedUser.get)
-
-                //TODO Use cookie for this, when I get Angular.js to work with responsecookies
-                Ok ~> ResponseString("{\"SESSION_ID\":\"" ++sessionId++ "\" }")
+                Ok ~> SetCookies(Cookie("SESSION_ID", sessionId))
               }
               else BadRequest ~> ResponseString("Wrong username or password")
             }
