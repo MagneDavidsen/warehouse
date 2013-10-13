e9afc6bdbd5718e0ce8edc3054c6a89a466a6ac4
Magne Davidsen <magne.davidsen@gmail.com>
2013-10-13 21:11:05 +0200

    stopped using liquibase for migrations

--- project/Build.scala
+++ project/Build.scala
@@ -9,15 +9,4 @@ object NbrnoBuild extends Build {
 
   lazy val environment = SettingKey[String]("environment", "environment variable")
 
-  lazy val root = Project(id = "nbrno",
-    base = file("."),
-    settings = Project.defaultSettings ++ liquibaseSettings ++
-      Seq(environment := realenv)++
-
-      Seq(liquibaseUsername := Properties.envOrElse("DB_USER", "clocking"),
-      liquibasePassword := Properties.envOrElse("DB_PASSWORD", "clocking"),
-      liquibaseDriver := "org.postgresql.Driver",
-      liquibaseUrl := "jdbc:postgresql://localhost:5432/nbrno",
-      liquibaseChangelog := "src/main/migrations/changelog.sql")
-  )
 }
