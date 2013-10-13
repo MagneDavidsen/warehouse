6e2165f861b74a42e355d0b8a560ec52159cee96
Magne Davidsen <magne.davidsen@gmail.com>
2013-10-13 21:14:17 +0200

    oops, messed up Build.scala

--- project/Build.scala
+++ project/Build.scala
@@ -9,4 +9,11 @@ object NbrnoBuild extends Build {
 
   lazy val environment = SettingKey[String]("environment", "environment variable")
 
+  lazy val root = Project(id = "nbrno",
+    base = file("."),
+    settings = Project.defaultSettings ++
+      Seq(environment := realenv)
+
+
+  )
 }
