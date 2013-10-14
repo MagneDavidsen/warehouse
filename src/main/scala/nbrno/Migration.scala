9126bb1508ddde30079fefb9ccb315417d945378
Magne Davidsen <magne.davidsen@gmail.com>
2013-10-14 16:32:58 +0200

    method for using  when available

--- src/main/scala/nbrno/Migration.scala
+++ src/main/scala/nbrno/Migration.scala
@@ -1,8 +1,5 @@
 package nbrno
 
-import javax.sql.DataSource
-import org.postgresql.ds.{PGSimpleDataSource}
-import scala.util.Properties
 import scala.slick.driver.PostgresDriver.simple._
 import nbrno.domain.Rapper
 import java.sql.Timestamp
@@ -10,16 +7,7 @@ import scala.slick.session.Database
 
 object Migration extends App {
 
-  val dataSource: DataSource = {
-    val ds = new PGSimpleDataSource
-    ds.setDatabaseName("nbrno")
-    ds.setUser(Properties.envOrElse("DB_USER", "clocking"))
-    ds.setPassword(Properties.envOrElse("DB_PASSWORD", "clocking"))
-    ds.setServerName("localhost")
-    ds.setPortNumber(5432)
-    ds
-  }
-
+  val dataSource = NbrnoServer.getDataSource
   val dbHandler : DatabaseHandler = new DatabaseHandler(dataSource)
 
   implicit val session = Database.forDataSource(dataSource).createSession()
--- src/main/scala/nbrno/NbrnoServer.scala
+++ src/main/scala/nbrno/NbrnoServer.scala
@@ -2,11 +2,10 @@ package nbrno
 
 import unfiltered.jetty.Http
 import scala.util.Properties
-import java.net.URL
+import java.net.{URI, URL}
 import scala.collection.{immutable}
 import java.util.UUID
 import nbrno.domain.User
-import scala.collection.immutable.HashMap
 import javax.sql.DataSource
 import org.postgresql.ds.{PGSimpleDataSource}
 
@@ -15,22 +14,40 @@ object NbrnoServer extends App {
   //token is key, username is value
   val sessionStore : SessionStore = new SessionStore(new immutable.HashMap[String, User])
 
-  val dataSource: DataSource = {
-    val ds = new PGSimpleDataSource
-    ds.setDatabaseName("nbrno")
-    ds.setUser(Properties.envOrElse("DB_USER", "clocking"))
-    ds.setPassword(Properties.envOrElse("DB_PASSWORD", "clocking"))
-    ds.setServerName("localhost")
-    ds.setPortNumber(5432)
-    ds
+  def getDataSource : DataSource = {
+    val databaseUrl : Option[String] = Option(System.getenv("DATABASE_URL"))
+    databaseUrl match{
+      case Some(value) => {
+        val dbUrl = new URI(value)
+        val ds = new PGSimpleDataSource
+        ds.setUser(dbUrl.getUserInfo.split(":"){0})
+        ds.setPassword(dbUrl.getUserInfo.split(":"){1})
+        ds.setServerName(dbUrl.getHost)
+        ds.setPortNumber(dbUrl.getPort)
+        ds.setDatabaseName(dbUrl.getPath.tail) //remove leading slash
+        ds
+      }
+      case None => {
+        //fallback for dev
+        val ds = new PGSimpleDataSource
+        ds.setDatabaseName("nbrno")
+        ds.setUser(Properties.envOrElse("DB_USER", "clocking"))
+        ds.setPassword(Properties.envOrElse("DB_PASSWORD", "clocking"))
+        ds.setServerName("localhost")
+        ds.setPortNumber(5432)
+        ds
+      }
+    }
   }
 
-  val dbHandler : DatabaseHandler = new DatabaseHandler(dataSource)
+  val dbHandler : DatabaseHandler = new DatabaseHandler(getDataSource)
 
   Http(Properties.envOrElse("PORT", "8081").toInt).resources(new URL(getClass().getResource("/www/"), "."))
     .filter(RappersPlan).filter(UserPlan).run()
 
 }
+
+
 
 class SessionStore(var map:immutable.HashMap[String, User]){
 
@@ -41,8 +58,7 @@ class SessionStore(var map:immutable.HashMap[String, User]){
   }
 
   def getUser(token : String) : Option[User] = map.get(token)
-
   def removeUser(token : String) = map-=(token)
-
   def size() : Int = map.size
 }
+
